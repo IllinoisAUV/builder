80ca20d78e15d880dcab0e7e90042a056f5ad764
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-02-06 13:33:26 -0600

    Make gcc for debs

---
+++ from-deb/make-gcc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# wget --quiet https://developer.nvidia.com/embedded/dlc/l4t-gcc-toolchain-64-bit-sources-28-1 -O /tmp/gcc-4.8.5-aarch64.tgz
+mkdir -p /tmp/gcc
+# tar -xf /tmp/gcc-4.8.5-aarch64.tgz -C /opt/gcc
+
+
+cd /opt/gcc
+mv /tmp/make-aarch64-toolchain.sh .
+# cat make-aarch64-toolchain.sh
+
+
+./make-aarch64-toolchain.sh
+
+cd /
+rm -rf /tmp/*
